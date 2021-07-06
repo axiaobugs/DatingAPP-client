7807081d1173f9007d007249b6fd1649c9bff99a
Yanxiao Fang <axiaobug@hotmail.com>
2021-07-06 22:47:57 +1000

    feat: (member-list,member.service) Resorting the caching for member detail and remembering the filter for a user in the service

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -3,9 +3,11 @@ import {environment} from "../../environments/environment";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Member} from "../_models/member";
 import {of, pipe} from "rxjs";
-import {map} from "rxjs/operators";
+import {map, take} from "rxjs/operators";
 import {PaginatedResult} from "../_models/pagination";
 import {UserParams} from "../_modules/userParams";
+import {AccountService} from "./account.service";
+import {User} from "../_models/user";
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +16,29 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   members:Member[] = [];
   memberCache = new Map();
+  user:User;
+  userParams:UserParams;
 
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private accountService:AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user=>{
+      this.user = user;
+      this.userParams = new UserParams(user);
+    })
+  }
+
+  getUserParams(){
+    return this.userParams;
+  }
+
+  setUserParams(params:UserParams){
+    this.userParams = params
+  }
+
+  resetUserParams(){
+    this.userParams = new UserParams(this.user);
+    return this.userParams;
+  }
 
   getMembers(userParams:UserParams){
     let response = this.memberCache.get(Object.values(userParams).join('-'));
@@ -58,8 +80,12 @@ export class MembersService {
   }
 
   getMember(username: string){
-    const member = this.members.find(x=>x.username === username);
-    if (member!== undefined) return of(member);
+    const member = [...this.memberCache.values()]
+      .reduce((arr,elem)=>
+        arr.concat(elem.result),[]
+      )
+      .find((member:Member)=>member.username===username)
+    if (member) return of(member)
     return this.http.get<Member>(this.baseUrl+'users/'+username)
   }
 
--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -19,11 +19,8 @@ export class MemberListComponent implements OnInit {
   user:User;
   genderList = [{value:'male',display:'Male'},{value:'female',display: 'Female'}]
 
-  constructor(private memberService:MembersService,private accountService:AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user=>{
-      this.user = user;
-      this.userParams = new UserParams(user);
-    })
+  constructor(private memberService:MembersService) {
+    this.userParams = this.memberService.getUserParams();
   }
 
   ngOnInit(): void {
@@ -31,21 +28,22 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers(){
+    this.memberService.setUserParams(this.userParams)
     this.memberService.getMembers(this.userParams).subscribe(response=>{
       this.members = response.result;
       this.pagination = response.pagination;
-      console.log(response.pagination)
     })
   }
 
   pageChanged(event:any){
     this.userParams.pageNumber = event.page;
+    this.memberService.setUserParams(this.userParams);
     this.loadMembers();
   }
 
   resetFilters(){
-    this.userParams = new UserParams(this.user);
-    this.loadMembers()
+    this.userParams = this.memberService.resetUserParams();
+    this.loadMembers();
   }
 
 }
